20c996dc04b98eb1f4827d10a314bfc8d8df881a
Aliakseyeu Raman <aliakseyeu.raman@gmail.com>
2023-03-23 11:43:37 +0300

    Upload MysqlDB structure

---
+++ db_structure.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, MetaData, String, CHAR, VARCHAR
+from database import Base
+
+
+metadata = MetaData()
+
+
+class Trainee(Base):
+    __tablename__ = "trainee"
+
+    ID = Column(CHAR(5), primary_key=True, nullable=True)
+    name = Column(VARCHAR(20), nullable=False)
+    dept_name = Column(VARCHAR(20))
+
+
+class Instructor(Base):
+    __tablename__ = "instructor"
+
+    ID = Column(CHAR(5), primary_key=True, nullable=True)
+    name = Column(VARCHAR(20), nullable=False)
+    dept_name = Column(VARCHAR(20))
